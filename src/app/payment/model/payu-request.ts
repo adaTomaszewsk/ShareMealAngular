0615eb43aa0123c7e0c29ec2b8fd3db669a570b7
Adamowik <56548965+Adamowik@users.noreply.github.com>
2020-09-27 19:31:12 +0200

    Poprawione platnosci i firebase
    Poprawione platnosci i firebase

---
+++ src/app/payment/model/payu-request.ts
@@ -0,0 +1,12 @@
+export class PayuRequest {
+    txnid: string;
+    surl: string;
+    furl: string;
+    key: string;
+    hash: string;
+    firstname: string;
+    productinfo: string;
+    amount: string;
+    email: string;
+    phone: string;
+  }
---
+++ src/app/payment/model/ticket-transport-model.ts
@@ -0,0 +1,7 @@
+export interface TicketTransportModel {
+    email: string;
+    name: string;
+    phone: string;
+    productInfo: string;
+    amount: string;
+  }
