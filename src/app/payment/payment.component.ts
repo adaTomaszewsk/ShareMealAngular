edbdfb3cce678b6f0c2e036a91f37f64d1bf29b1
Adamowik <56548965+Adamowik@users.noreply.github.com>
2020-09-27 19:14:37 +0200

    Poprawione_platnosci_firebase

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -2,11 +2,6 @@ import { FlightSearchService } from './../services/flight-search.service';
 import { Component, OnInit } from '@angular/core';
 import { DictionaryService } from '../services/dictionary.service';
 
-@Component({
-  selector: 'app-payment',
-  templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
-})
 export class PaymentComponent implements OnInit {
 
   constructor(public flightSearchService: FlightSearchService, public dictionary: DictionaryService) { }
